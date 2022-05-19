d5bf0ea6aabdf66e684fd5b8cd056e5b8a61ee4c
pappy <pa314159@users.noreply.github.com>
2022-05-19 16:14:21 +0300

    PRSUM-8157: upgraded to 2021.11

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM harbor.backbase.eu/staging/arrangement-manager:DBS-2.20.0-cr.124
-
-ARG JAR_FILE
-COPY target/${JAR_FILE} /app/WEB-INF/lib/
--- src/main/java/com/backbase/dbs/product/arrangement/ExtendedPermissionAwareArrangementService.java
+++ src/main/java/com/backbase/dbs/product/arrangement/ExtendedPermissionAwareArrangementService.java
@@ -2,6 +2,7 @@ package com.backbase.dbs.product.arrangement;
 
 import com.backbase.buildingblocks.backend.security.accesscontrol.accesscontrol.AccessControlValidator;
 import com.backbase.dbs.product.Configurations;
+import com.backbase.dbs.product.ProductKindStorage;
 import com.backbase.dbs.product.balance.BalanceService;
 import com.backbase.dbs.product.clients.AccessControlClient;
 import com.backbase.dbs.product.clients.JwtContext;
@@ -9,33 +10,31 @@ import com.backbase.dbs.product.common.clients.AccountOutboundWrapper;
 import com.backbase.dbs.product.repository.ArrangementJpaRepository;
 import com.backbase.dbs.product.repository.ProductJpaRepository;
 import com.backbase.dbs.product.repository.arrangement.ArrangementSearchParams;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Locale;
+import javax.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-
-import javax.validation.constraints.NotNull;
-import java.util.Locale;
 
 /**
  * Sample class extending getArrangementsByBusinessFunction operation.
  */
 @Primary
 @Service
+@Slf4j
 public class ExtendedPermissionAwareArrangementService extends PermissionAwareArrangementService {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ExtendedPermissionAwareArrangementService.class);
     private static final String[] LOCALES = Locale.getISOCountries();
 
-    public ExtendedPermissionAwareArrangementService(
-            Configurations configurations, ArrangementService arrangementService, JwtContext jwtContext,
-            AccessControlClient accessControlClient, AccountOutboundWrapper accountOutboundWrapper,
-            ArrangementUpdater arrangementUpdater, ArrangementJpaRepository arrangementJpaRepository,
-            ProductJpaRepository productJpaRepository, BalanceService balanceService,
-            AccessControlValidator accessControlValidator, DebitCardCommandMapper debitCardCommandMapper) {
-        super(configurations, arrangementService, jwtContext, accessControlClient, accountOutboundWrapper,
-                arrangementUpdater, arrangementJpaRepository, productJpaRepository,
-                balanceService, accessControlValidator, debitCardCommandMapper);
+    public ExtendedPermissionAwareArrangementService(Configurations configurations, ProductKindStorage kindStorage,
+        ArrangementService arrangementService, JwtContext jwtContext, AccessControlClient accessControlClient,
+        AccountOutboundWrapper accountOutboundWrapper, ArrangementUpdater arrangementUpdater,
+        ArrangementJpaRepository arrangementJpaRepository, ProductJpaRepository productJpaRepository,
+        BalanceService balanceService, AccessControlValidator accessControlValidator,
+        DebitCardCommandMapper debitCardCommandMapper) {
+        super(configurations, kindStorage, arrangementService, jwtContext, accessControlClient, accountOutboundWrapper,
+            arrangementUpdater, arrangementJpaRepository, productJpaRepository, balanceService, accessControlValidator,
+            debitCardCommandMapper);
         LOG.info("Extended service has been created");
     }
 
@@ -49,7 +48,7 @@ public class ExtendedPermissionAwareArrangementService extends PermissionAwareAr
 
         LOG.info("Pre hook");
 
-        final GetArrangementsByBusinessFunctionResult result = super.getArrangementsByBusinessFunction(
+        final var result = super.getArrangementsByBusinessFunction(
             arrangementSearchParams, withLatestBalances, privilege, businessFunction, resourceName);
 
         LOG.info("Post hook");
@@ -64,11 +63,12 @@ public class ExtendedPermissionAwareArrangementService extends PermissionAwareAr
      */
     private void updateHolderLocationInfo(GetArrangementsByBusinessFunctionResult result) {
         result.getArrangements().forEach(arrangement -> {
-            final String countryName = new Locale("en",
+            final var countryName = new Locale("en",
                 LOCALES[(int) (Math.random() * LOCALES.length)]).getDisplayCountry();
 
             arrangement.setAccountHolderCountry(countryName);
             arrangement.setAccountHolderStreetName("123 Fake Street");
+
             LOG.info("Update holder location updated to {} {}",
                 arrangement.getAccountHolderCountry(), arrangement.getAccountHolderStreetName());
         });
